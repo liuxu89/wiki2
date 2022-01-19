81c62ce4d94a1a2badbc9681f529577bc29293c7
Documenter.jl <documenter@juliadocs.github.io>
2022-01-19 14:57:50 +0000

    build based on 8a23a3b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = wiki2","category":"page"},{"location":"#wiki2","page":"Home","title":"wiki2","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for wiki2.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [wiki2]","category":"page"}]
+}
