f32ae2e29f1167815f15b243238659904db95cf1
liuxu <liuxu89@mail.ustc.edu.cn>
2022-01-19 23:11:20 +0800

    ci

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(wiki2, :DocTestSetup, :(using wiki2); recursive=true)
 makedocs(;
     modules=[wiki2],
     authors="liuxu <liuxu89@mail.ustc.edu.cn> and contributors",
-    repo="https://github.com/liuxu89/wiki2.jl/blob/{commit}{path}#{line}",
-    sitename="wiki2.jl",
+    repo="https://github.com/liuxu89/wiki2/blob/{commit}{path}#{line}",
+    sitename="wiki2",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://liuxu89.github.io/wiki2.jl",
+        canonical="https://liuxu89.github.io/wiki2",
         assets=String[],
     ),
     pages=[
@@ -19,6 +19,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/liuxu89/wiki2.jl",
+    repo="github.com/liuxu89/wiki2",
     devbranch="main",
 )
